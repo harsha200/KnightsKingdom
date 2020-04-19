52be08187aed648e55476479bafba2fee146641c
harsha200 <33797987+harsha200@users.noreply.github.com>
2020-04-19 03:05:19 +0100

    SpringArm Camera Rotation Functionality added to the collider

--- Source/KnightsKingdom/Collider.h
+++ Source/KnightsKingdom/Collider.h
@@ -63,4 +63,9 @@ public:
 private:
 	void MoveForward(float input);
 	void MoveRight(float input);
+
+	void PitchCamera(float AxisValue);
+	void YawCamera(float AxisValue);
+
+	FVector2D CameraInput;
 };
