d46ada2b857818e5e9f37d56e68344f76048edf2
harsha200 <33797987+harsha200@users.noreply.github.com>
2020-04-22 17:13:33 +0100

    AnimInstance is created for knight character

---
+++ Source/KnightsKingdom/MainAnimInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KNIGHTSKINGDOM_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
+
+
+	UPROPERTY(EditAnywhere , BlueprintReadOnly , category = Movement)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere , BlueprintReadOnly , category = Movement)
+	bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = Movement)
+	class APawn* Pawn;
+};
